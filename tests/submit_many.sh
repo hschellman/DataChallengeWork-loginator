68e9675808abd5fd56d8af9fb5648f76cd236acb
Heidi Schellman <Heidi.Schellman@oregonstate.edu>
2022-12-27 13:41:36 -0800

    not certain why gitadd did not add these

--- tests/submit_many.sh
+++ tests/submit_many.sh
@@ -1,6 +1,7 @@
 # submission tests
 source tarme.sh  # make the tar file up-to-date
-python submit_dd_jobs.py --dataset=schellma:run5141recentReco  --namespace=pdsp_det_reco --query_limit=30 --fcl=test.fcl --njobs=50 --nevents=20 --load_limit=2 --appFamily=LArSoft --appName=test --appVersion=${DUNESW_VERSION}
+python -m submit_dd_jobs --dataset=schellma:run5141recentReco  --namespace=pdsp_det_reco --query_limit=50\
+ -c eventdump.fcl --njobs=35 -n -1 --load_limit=2 --appFamily=LArSoft --appName=test --appVersion=${DUNESW_VERSION}
 
 #--dataset DATASET] [--namespace NAMESPACE] [--query_limit QUERY_LIMIT]
 #                         [--query_skip QUERY_SKIP] [--njobs NJOBS] [--load_limit LOAD_LIMIT] [--fcl FCL] [--nevents NEVENTS]
