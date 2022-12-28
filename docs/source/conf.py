157fb0a5b0013029b362f22d254b443015d1d53a
Heidi Schellman <Heidi.Schellman@oregonstate.edu>
2022-12-28 14:21:46 -0800

    add a mock import line to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,6 +63,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ["metacat","samweb_client","data_dispatcher"]
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
